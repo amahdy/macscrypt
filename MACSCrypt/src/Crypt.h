2a2275036943405904a1f88c990ef41d091446e9
amahdy <amahdy7@gmail.com>
2009-03-01 18:18:37 +0000

    Crypt.h updated

--- MACSCrypt/src/Crypt.h
+++ MACSCrypt/src/Crypt.h
@@ -1,12 +1,22 @@
 #ifndef CRYPT
 #define CRYPT
 
-class Crypt
-{
+/**
+ * ==============================================
+ * @name CRYPT ABSTRACT CLASS
+ * @desc This is the abstract cryptography class, from here inherit any algorithm class.
+ * @data empty
+ * @member empty
+ * @public %1 void: key generator function.
+ *         %2 char*: encrypt function.
+ *         %3 char*: decrypt function.
+ * ==============================================
+ */
+class Crypt {
 public:
-	virtual void KeyGen(const char*)=0;
-	virtual char* Encrypt(char*)=0;
-	virtual char* Decrypt(char*)=0;
+    virtual void KeyGen(const char*)=0;
+    virtual char* Encrypt(char*)=0;
+    virtual char* Decrypt(char*)=0;
 };
 
 #endif
