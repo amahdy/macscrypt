ee20b98ca7e932a6277342cea2b11ba7ee6e64f9
mrjava.javaman <mrjava.javaman@0051ba18-f7b4-11dd-9fdb-3debd5008f28>
2009-03-01 19:02:35 +0000

    DES demo

---
+++ MACSCrypt_DES/main.cpp
@@ -0,0 +1,50 @@
+/* 
+ * File:   main.cpp
+ * Author: Amahdy
+ *
+ * Created on March 1, 2009, 8:23 PM
+ */
+
+#include <stdlib.h>
+#include <iostream>
+#include "des.h"
+
+/*
+ * displayer
+ */
+void disp(char* data, int length) {
+    for (int i = 0; i < length; i++) {
+        std::cout<<data[i];
+    }
+}
+
+/*
+ * entry point
+ */
+int main(int argc, char** argv) {
+    char HEX16_KEY[]="0123456789ABCDEF";
+    int NUMBER_OF_ROUNDS=16;
+    CDES DES(NUMBER_OF_ROUNDS);
+    DES.KeyGen(HEX16_KEY);
+
+    char* PLAINTEXT;
+    char* CYPHERTEXT;
+
+    PLAINTEXT = new char[17];
+    PLAINTEXT = "0123456789ABCDEF";
+
+    CYPHERTEXT = DES.Encrypt(PLAINTEXT);
+    std::cout<<"Cypher: ";
+    disp(CYPHERTEXT, 17);
+    std::cout<<std::endl;
+
+//    CYPHERTEXT = new char[17];
+//    CYPHERTEXT = "0123456789ABCDEF";
+
+    PLAINTEXT = DES.Decrypt(CYPHERTEXT);
+    std::cout<<"Plain: ";
+    disp(PLAINTEXT, 17);
+    std::cout<<std::endl;
+    
+    return (EXIT_SUCCESS);
+}
