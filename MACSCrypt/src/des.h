2018b6ef02c7f15245b605b5296a6cd783b97220
mrjava.javaman <mrjava.javaman@0051ba18-f7b4-11dd-9fdb-3debd5008f28>
2009-03-01 16:09:49 +0000

    des.cpp des.h updated

--- MACSCrypt/src/des.h
+++ MACSCrypt/src/des.h
@@ -1,50 +1,63 @@
 /*###################################
- #  By : Amahdy [www.amahdy.com]    #
- ###################################*/
+  # DES [Data Encryption Standard]
+  # By: Amahdy [www.amahdy.com]
+  ###################################*/
 
 #ifndef DESALGORITHM
 #define DESALGORITHM
 
-/*###################################
- #  DES "Data Encryption Standard"  #
- #      ====ByAmahdy====            #
- #	[www.amahdy.com]            #
- ###################################*/
+/**
+ * ==============================================
+ * @name DES CLASS
+ * @desc Class definition for the DES algorithm.
+ * @data %1 char**: The expanded key.
+ *       %2 char[8][4][16]: the S-BOX.
+ *       %3 int: number of rounds.
+ * @member %1 char*: the main function.
+ *         %2 void: hex to bin converter.
+ *         %3 void: bin to hex converter.
+ *         %4 void: dec to bin converter.
+ *         %5 void: bin to dec converter.
+ *         %6 void: rotation (shifting).
+ *         %7 void: xor funciton.
+ * @public %1 void: constructor, feed it with the number of rounds.
+ *         %2 void: destructor, never mind ... called automatically.
+ *         %3 void: key generator, should call this before anything to generate keys.
+ *         %4 char*: call this to encrypt.
+ *         %5 char*: call this to decrypt.
+ * ==============================================
+ */
+class CDES:public Crypt {
+    char **keyEx;
+    char s[8][4][16];
+    int numberOfRounds;
+    char* DES(char*, bool=false);
 
-class CDES:public Crypt
-{
-	char **keyEx; //expanded key
-	char s[8][4][16]; //s-boxs
-	int numberOfRounds; //number of rounds to execute the algorithm
-	char* DES(char*,bool=false); //GO from here !
-
-	//additional functions:
-	/*
-	void HexToBin(const char*, char*);
-	void BinToHex(const char*, char*);
-	*/
-	void DecToBin(const char*, char*);
-	void BinToDec(const char*, char*);
-	void rotation(char*, int);
-	void exor(char*, char*, char*, int);
+    //void HexToBin(const char*, char*);
+    //void BinToHex(const char*, char*);
+    void DecToBin(const char*, char*);
+    void BinToDec(const char*, char*);
+    void rotation(char*, int);
+    void exor(char*, char*, char*, int);
 public:
-	CDES(int=16); //consturctor, feed it with the number of rounds
-	~CDES(); //destructor, never mind ... called automatically
-	void KeyGen(const char*); //should call this before anything to generate keys
-	char* Encrypt(char*); //Call this to encrypt
-	char* Decrypt(char*); //Call this to decrypt
+    CDES(int=16);
+    ~CDES();
+    void KeyGen(const char*);
+    char* Encrypt(char*);
+    char* Decrypt(char*);
 
-/*
-	>sample encryption:
-	CDES DES(NUMBER_OF_ROUNDS);
-	DES.KeyGen(16HEX_KEY);
-	OUTPUT=DES.Encrypt(PLAINTEXT);
-
-	>sample decryption:
-	CDES DES(NUMBER_OF_ROUNDS);
-	DES.KeyGen(16HEX_KEY);
-	OUTPUT=DES.Decrypt(CYPHERTEXT);
-*/
+/**
+ * >sample encryption:
+ *      CDES DES(NUMBER_OF_ROUNDS);
+ *      DES.KeyGen(16HEX_KEY);
+ *      OUTPUT=DES.Encrypt(PLAINTEXT);
+ *
+ * >sample decryption:
+ *      CDES DES(NUMBER_OF_ROUNDS);
+ *      DES.KeyGen(16HEX_KEY);
+ *      OUTPUT=DES.Decrypt(CYPHERTEXT);
+ */
 
 };
+
 #endif
